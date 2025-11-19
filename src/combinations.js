40a82f9ca84c199a0102e580c1844e951fc04023
emmanuel3690 <emmanuelinnn13690@gmail.com>
2025-11-19 10:53:53 -0800

    Updated combinations.js and SacredNumbers component

--- src/combinations.js
+++ src/combinations.js
@@ -1,5 +1,3 @@
-// combinations.js
-
 const combinations = [
   {
     row: 1,
@@ -13,137 +11,142 @@ const combinations = [
     row: 3,
     items: ["1+4+5"],
   },
-  { 
+
+  {
     row: 4,
+    items: ["1+7+10"],
+  },
+  { 
+    row: 5,
     items: ["1+8+9"],
   },
   {
-    row: 5,
+    row: 6,
     items: ["1+11+14"],
   },
   {
-    row: 6,
+    row: 7,
     items: ["1+12+13"],
   },
 
   // ROW 7–11
   {
-    row: 7,
+    row: 8,
     items: ["2+4+6"],
   },
   {
-    row: 8,
+    row: 9,
     items: ["2+7+9"],
   },
   {
-    row: 9,
+    row: 10,
     items: ["2+8+10"],
   },
   {
-    row: 10,
+    row: 11,
     items: ["2+11+13"],
   },
   {
-    row: 11,
+    row: 12,
     items: ["2+12+14"],
   },
 
   {
-   row: 12,
+   row: 13,
  items: ["3+2+5"],
 },
 
 {
-  row: 13,
+  row: 14,
   items :["3+4+15"],
 },
 {
-  row: 14,
+  row: 15,
   items: ["3+7+12"],
 },
 
 {
-  row: 15,
+  row: 16,
   items: ["3+8+11"],
 },
 {
-  row: 16,
+  row: 17,
   items: ["3+9+14"],
 },
 {
-  row: 17,
+  row: 18,
   items: ["3+10+13"],
 },
   // ROW 12–15
   {
-    row: 18,
+    row: 19,
     items: ["4+7+11"],
   },
   {
-    row: 19,
+    row: 20,
     items: ["4+8+12"],
   },
   {
-    row: 20,
+    row: 21,
     items: ["4+9+13"],
   },
   {
-    row: 21,
+    row: 22,
     items: ["4+10+14"],
   },
 
   // ROW 16–20
   {
-    row: 22,
+    row: 23,
     items: ["5+6+15"],
   },
   {
-    row: 23,
+    row: 24,
     items: ["5+7+14"],
   },
   {
-    row: 24,
+    row: 25,
     items: ["5+8+13"],
   },
   {
-    row: 25,
+    row: 26,
     items: ["5+9+12"],
   },
   {
-    row: 26,
+    row: 27,
     items: ["5+10+11"],
   },
 
   {
-  row: 27,
+  row: 28,
   items: ["6+7+13"],
 },
 {
-  row: 28,
+  row: 29,
   items: ["6+8+14"],
 },
 {
-  row: 29,
-  items: ["6+9+11"],
-},
-{
   row: 30,
-  items: ["6+10+12"],
+  items: ["6+9+12"],
 },
 {
   row: 31,
-  items: ["7+8+15"],
+  items: ["6+10+12"],
 },
 {
   row: 32,
-  items: ["9+10+15"],
+  items: ["7+8+15"],
 },
 {
   row: 33,
-  items: ["12+15+11"],
+  items: ["9+10+15"],
 },
 {
   row: 34,
+  items: ["12+15+11"],
+},
+{
+  row: 35,
   items: ["13+14+15"],
 },
 
